5b124478a3f0a7dc9cc57fa74d12872408c0f1ef
Username-3813 <Kirak90@mail.ru>
2026-07-15 17:55:12 +0400

    Обновлено 15.07.2026

--- src/main/java/com/garage/ElectronicGarageApplication.java
+++ src/main/java/com/garage/ElectronicGarageApplication.java
@@ -2,12 +2,10 @@ package com.garage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ElectronicGarageApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElectronicGarageApplication.class, args);
-       //System.out.println(new BCryptPasswordEncoder().encode("admin123"));
     }
 }
